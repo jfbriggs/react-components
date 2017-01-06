af9c695dbe3bccb369b518ccd85f5283f4f218c1
James Briggs <jfbriggs106@gmail.com>
2017-01-06 11:34:03 -0800

    Add reusable item component

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 // TODO
 
+let groceryList = ['Cheese', 'Crackers']
+
 let GroceryList = () => (
   <ul>
-    <Cheese />
-    <Crackers />
+    <GroceryListItem item={groceryList[0]}/>
+    <GroceryListItem item={groceryList[1]}/>
   </ul>
 );
 
@@ -13,6 +15,10 @@ let Cheese = () => (
 
 let Crackers = () => (
   <li>Crackers</li>
+);
+
+let GroceryListItem = (props) => (
+  <li>{props.item}</li>
 );
 
 ReactDOM.render(<GroceryList />, document.getElementById("app"));
